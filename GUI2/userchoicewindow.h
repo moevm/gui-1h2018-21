8fe760fc1ebef12c8a5874ee405b3dffec740083
MaximGaskov <maskimgaskov@mail.ru>
2018-04-12 00:11:31 +0300

    Update userchoicewindow.h

--- GUI2/userchoicewindow.h
+++ GUI2/userchoicewindow.h
@@ -2,11 +2,40 @@
 #define USERCHOICEWINDOW_H
 
 #include <QWidget>
+#include <string>
+#include <QScrollArea>
+#include <QLayout>
+#include <QPushButton>
 
-class UserChoiceWindow
+namespace Ui {
+class UserChoiceWindow;
+}
+
+class UserChoiceWindow : public QWidget
 {
-public:
-    UserChoiceWindow();
-};
+    Q_OBJECT
 
+public:
+    explicit UserChoiceWindow(QWidget *parent = nullptr);
+    void addUserNameButton(std::string userName);
+    ~UserChoiceWindow();
+
+private slots:
+    void addUserButtonClicked();
+    void userNameButtonClicked();
+
+signals:
+    void addUserButtonSignal();
+    void userNameButtonSignal();
+
+private:
+    Ui::UserChoiceWindow *ui;
+    QPixmap _pixmapBg;
+    QWidget *scrollAreaWidget;
+    QScrollArea *scrollArea;
+    QVBoxLayout *scrollAreaWidgetLayout;
+    QPushButton *newUserButton;
+    std::string clickedUserName;
+    void paintEvent(QPaintEvent *e);
+};
 #endif // USERCHOICEWINDOW_H
